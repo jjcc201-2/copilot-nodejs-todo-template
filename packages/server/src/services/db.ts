d57daad65ea92b210c1b20382695c8dd54471073
jjcc201-2 <59102262+jjcc201-2@users.noreply.github.com>
2023-09-12 09:30:12 +0000

    add additional boilerplate for demos

---
+++ packages/server/src/services/db.ts
@@ -0,0 +1,5 @@
+// Import Cosmos SDK and task model
+
+// Create a DbService class to wrap the Cosmos SDK,
+// connecting to the 'todos' database and 'tasks' container
+// and with CRUD methods for tasks
